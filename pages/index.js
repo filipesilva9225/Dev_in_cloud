fedb868bfa533f78153cbc11dc2e7d1c94c9b9f0
Filipe Silva <140545021+filipesilva9225@users.noreply.github.com>
2025-03-26 14:09:27 +0000

    subtitulo

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 function Home(){
-    return <h1>Sophia, te amo meu amorzão ❤❤</h1>
+    return <h1>Sophia, te amo meu amorzão ❤❤
+        <h2>quero te veeer</h2>
+    </h1>
+                
 }
 export default Home;
