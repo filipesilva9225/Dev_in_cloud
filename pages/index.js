c4b6ecb0b7b9e3ad7c7740ba491d4dc98dd2ad26
Filipe Silva <140545021+filipesilva9225@users.noreply.github.com>
2025-03-26 14:06:53 +0000

    teste 1

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 function Home(){
-    return <h1>Sophia, te amo meu amorzão ❤❤</h1><h2>quero te ver amanhã!</h2>
-}
+    return <h1>Sophia, te amo meu amorzão ❤❤</h1>
 
-export default Home;
+export default Home
